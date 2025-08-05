36e480db7d6ba1009610cd49dd62286afda18cb0
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-08-05 00:13:31 +0000

    [feat] feito tela de login e cadastro - Bianca

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,14 @@ import {
   Image,
   TouchableOpacity,
   Pressable,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
+import logo from '../assets/imagens/logorestoon.png';
+import { Ionicons } from "@expo/vector-icons";
+import { router } from 'expo-router';
+
 
 export default function App() {
   const [email, setEmail] = useState('');
@@ -16,83 +23,93 @@ export default function App() {
   const [mostrarSenha, setMostrarSenha] = useState(false);
 
   return (
-    <View style={styles.container}>
-      {/* LOGO */}
-      {/* <View style={styles.logoContainer}>
-        <Image
-          source={require('./assets/logo.png')} // Substitua pelo caminho correto da logo
-          style={styles.logo}
-          resizeMode="contain"
-        />
-      </View> */}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={styles.caixaprincipal}>
+          <View style={styles.logoContainer}>
+            <Image
+              source={logo}
+              style={styles.logo}
+              resizeMode="contain"
+            />
+          </View>
 
-      <View style={styles.formulariocaixa}>
-        <Text style={styles.titulo}>Email</Text>
-        <TextInput
-          value={email}
-          onChangeText={setEmail}
-          placeholder="restoon@gmail.com"
-          keyboardType="email-address"
-          style={styles.input}
-          placeholderTextColor="#888"
-        />
+          <View style={styles.formulariocaixa}>
+            <Text style={styles.titulo}>Email</Text>
+            <TextInput
+              value={email}
+              onChangeText={setEmail}
+              placeholder="restoon@gmail.com"
+              keyboardType="email-address"
+              style={styles.input}
+              placeholderTextColor="#888"
+            />
 
-        <Text style={styles.titulo}>Senha</Text>
-        <View style={styles.senha}>
-          <TextInput
-            value={senha}
-            onChangeText={setSenha}
-            placeholder="senha"
-            secureTextEntry={!mostrarSenha}
-            style={styles.caixa}
-            placeholderTextColor="#888"
-          />
-          <TouchableOpacity onPress={() => setMostrarSenha(!mostrarSenha)}>
-            <Text style={styles.mostrarsenha}>{mostrarSenha ? '🙈' : '👁️'}</Text>
-          </TouchableOpacity>
+            <Text style={styles.titulo}>Senha</Text>
+            <View style={styles.senha}>
+              <TextInput
+                value={senha}
+                onChangeText={setSenha}
+                placeholder="senha"
+                secureTextEntry={!mostrarSenha}
+                style={styles.caixa}
+                placeholderTextColor="#888"
+              />
+              <TouchableOpacity onPress={() => setMostrarSenha(!mostrarSenha)}>
+                 <Ionicons
+                   name={mostrarSenha ? "eye-outline" : "eye-off-outline"}
+                    size={24}
+                    color="#555"
+                 />
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.esquecisenha}>
+              <Text style={styles.link}>esqueci a senha</Text>
+            </TouchableOpacity>
+
+            <Pressable style={styles.botaologin} onPress={() => console.log('Login')}>
+              <Text style={styles.textologin}>LOGIN</Text>
+            </Pressable>
+
+            <View style={styles.rodape}>
+              <Text style={styles.textorodape}>É novo por aqui?</Text>
+              <TouchableOpacity onPress={() => router.push('/cadastrar')}>
+                <Text style={styles.link}> se cadastre já</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <StatusBar style="auto" />
         </View>
-
-        <TouchableOpacity style={styles.esquecisenha}>
-          <Text style={styles.link}>esqueci a senha</Text>
-        </TouchableOpacity>
-
-        <Pressable style={styles.botaologin} onPress={() => console.log('Login')}>
-          <Text style={styles.textologin}>LOGIN</Text>
-        </Pressable>
-
-        <View style={styles.rodape}>
-          <Text style={styles.textorodape}>É novo por aqui?</Text>
-          <TouchableOpacity onPress={() => console.log('Cadastrar')}>
-            <Text style={styles.link}> se cadastre já</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <StatusBar style="auto" />
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  caixaprincipal: {
     flex: 1,
-    backgroundColor: '#0052cc',
+    backgroundColor: '#004aad',
   },
   logoContainer: {
-    flex: 1.2,
+    flex: 3.7,
     alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
-    width: 250,
-    height: 90,
+    width: 330,
+    height: 140,
   },
   formulariocaixa: {
     flex: 2,
     backgroundColor: '#fff',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    padding: 24,
+    padding: 23,
   },
   titulo: {
     fontSize: 14,
