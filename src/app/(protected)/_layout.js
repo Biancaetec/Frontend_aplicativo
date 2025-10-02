54c437c15ef52ff61f402bfe0ae7e416a48a6ab8
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-10-02 11:40:15 +0000

    [feat]atualizando dependencias

--- src/app/(protected)/_layout.js
+++ src/app/(protected)/_layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';    
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
